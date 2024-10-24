188f87ae0d8ef194ddc0f824dae2c94e561450bb
ssaroj143 <ssarojkumarmishra@gmail.com>
2024-10-24 18:27:58 +0530

    ACDS-279883 bug text-spacing -fixed

--- src/hypothesis_testing_identifying_h0_and_h1/index.js
+++ src/hypothesis_testing_identifying_h0_and_h1/index.js
@@ -2,10 +2,21 @@ import "react-app-polyfill/ie11"; //added pollyfill suppourt  latest ES feature 
 import "react-app-polyfill/stable"; //added pollyfill to suppourt all stable ES features across browser.
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { StyleObserverProvider } from '../app/helpers/StyleObserverContext'
 import App from "./app";
 
 require("webpack-jquery-ui");
 require("jquery-ui-touch-punch");
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+const MainApp = () => {
+  return (
+    <>
+      <StyleObserverProvider />
+      <App />
+    </>
+  );
+};
+
+ReactDOM.render(<MainApp />, document.querySelector("#root"));
--- src/measures_of_center/index.js
+++ src/measures_of_center/index.js
@@ -3,9 +3,19 @@ import "react-app-polyfill/stable"; //added pollyfill to suppourt all stable ES 
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { StyleObserverProvider } from '../app/helpers/StyleObserverContext'
 import App from "./app";
 
 require("webpack-jquery-ui");
 require("jquery-ui-touch-punch");
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const MainApp = () => {
+  return (
+    <>
+      <StyleObserverProvider />
+      <App />
+    </>
+  );
+};
+
+ReactDOM.render(<MainApp />, document.querySelector("#root"));
